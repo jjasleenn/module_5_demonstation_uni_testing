7b8e1d651e59f607053181a103be61fee94e41b7
jasleen kaur <jjasleenkaur2@rrc.ca>
2024-10-22 13:44:16 -0500

    initial commit: set up the directory and structure as well as executing the functions with unit testing

---
+++ tests/test_functions.py
@@ -0,0 +1,15 @@
+"""
+Description: Module 05 demonstration: Functions with Unit Testing
+Author: ACE Faculty
+Date: {current date}
+Usage: To execute the unit tests: 
+        From the unit_testing directory in the Terminal:
+        python -m unittest -v tests/test_functions.py
+    To execute the python src program:
+        From the unit_testing directory in the Terminal:
+        python src/functions.py
+"""
+
+import unittest
+from unittest.mock import patch
+
